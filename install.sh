a5d609fd22ae1b131ce996c9043e1f9473ce7fd6
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-08-22 21:21:35 +0200

    1

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-SPLIT=$(printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -)
-
-apt update
-clear
+apt update &> /dev/null
 
 bash ./bash/docker_install.sh &> /dev/null
 
@@ -12,5 +9,3 @@ docker network create proxy_net
 bash ./bash/prometheus.sh &> /dev/null
 cp ./json/* /home/"${SUDO_USER:-$USER}"/docker/grafana/dashboards/
 bash ./bash/docker_compose.sh
-bash ./bash/log.sh
-cat log.txt
