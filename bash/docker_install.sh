f5eceaf2139bcc9927924704f4e726a117a9ac08
Senad Dizdarevic <96924112+sekkigit@users.noreply.github.com>
2022-08-29 10:11:18 +0200

    1

--- bash/docker_install.sh
+++ bash/docker_install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 yum update
-yum install docker.io -y
-yum install docker-compose -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+sudo usermod -a -G docker ec2-user
+sudo chmod +x /usr/local/bin/docker-compose
+
 mkdir -p /etc/docker/{prometheus,cadvisor}
 mkdir -p /etc/docker/grafana{dashboards,datasources}
-groupadd --system dockergroup
-useradd --system --no-create-home --group dockergroup,"$USER" -s /bin/false docker
-chown -R "$USER":docker /home/"$USER"/docker
-usermod -aG docker,adm "$USER"
